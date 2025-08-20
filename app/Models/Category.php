84f53ae4fb45fc7b8d002d69bed8636e7a7f093d
mahdirnma <rahnamamahdi78@gmail.com>
2025-08-20 20:04:54 +0330

    fill models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable=[
+        'title',
+        'description',
+        'is_active',
+    ];
+
+    public function post()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Database\Seeders\CategorySeeder;
+use Database\Seeders\TagSeeder;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable=[
+        'title',
+        'description',
+        'category_id',
+        'is_active',
+    ];
+    public function category(){
+        return $this->belongsTo(CategorySeeder::class);
+    }
+    public function tags(){
+        return $this->belongsToMany(TagSeeder::class);
+    }
 }
